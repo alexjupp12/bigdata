90f7c0f1831bcde2bc2e6672f29f2048ef581b60
Alex Lemus Colorado <98492829+alexjupp12@users.noreply.github.com>
2025-02-28 07:51:17 -0500

    Update Generar Datos.py

--- Generar Datos.py
+++ Generar Datos.py
@@ -1,1 +1,34 @@
+import random
+
+def generar_insert_sql(nombre_archivo="Data.sql", cantidad=50000):
+    try:
+        with open(nombre_archivo, "w") as archivo:
+            archivo.write("INSERT INTO xprueba.sensor VALUES\n")
+            
+            valores_lista = []
+            for _ in range(cantidad):
+                temp = random.randint(1, 99)
+                hr = random.randint(1, 11)
+                pat = random.randint(16, 26)
+                alt = random.randint(16, 26)
+                iuv = random.randint(16, 26)
+                lov = random.randint(16, 26)
+                loi = random.randint(16, 26)
+                pll = random.randint(1, 99)
+                vv = random.randint(1, 11)
+                dv = random.randint(16, 26)
+                nc = random.randint(16, 26)
+                il = random.randint(16, 26)
+                
+                valores = f"(null, current_timestamp, {temp}, {hr}, {pat}, {alt}, {iuv}, {lov}, {loi}, {pll}, {vv}, {dv}, {nc}, {il}, 1)"
+                valores_lista.append(valores)
+            
+            archivo.write(",\n".join(valores_lista) + ";\n")
+        
+        print("Archivo SQL generado con éxito.")
+    except Exception as e:
+        print(f"Error al escribir el archivo: {e}")
+
+if __name__ == "__main__":
+    generar_insert_sql()
 
